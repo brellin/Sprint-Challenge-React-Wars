9c02c7a56772a8385c5451266dea0610edcbaf71
brellin <cekakid@gmail.com>
2019-03-22 11:36:30 -0500

    styling

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,11 +5,21 @@ const Character = props => {
     console.log(props.char)
     return (
         <div className='char'>
-            <div className='data name'>{props.char.name}</div>
-            <div className='data height'>{props.char.height}in</div>
-            <div className='data mass'>{props.char.mass}lb</div>
-            <div className='data hair_color'>{props.char.hair_color}</div>
-            <div className='data skin_color'>{props.char.skin_color}</div>
+            <fieldset className='data name'>
+                <legend>Name:</legend>
+                {props.char.name}</fieldset>
+            <fieldset className='data height'>
+                <legend>Height:</legend>
+                {props.char.height}in</fieldset>
+            <fieldset className='data mass'>
+                <legend>Weight:</legend>
+                {props.char.mass}lb</fieldset>
+            <fieldset className='data hair_color'>
+                <legend>Hair Color:</legend>
+                {props.char.hair_color}</fieldset>
+            <fieldset className='data skin_color'>
+                <legend>Skin Color:</legend>
+                {props.char.skin_color}</fieldset>
         </div>
     )
 }
